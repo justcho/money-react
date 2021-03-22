5d6ac1e38c00b9e30c5f5b4674b0e50c406592bd
justchoe <1620845677@qq.com>
2021-03-22 15:39:48 +0800

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import './App.css';
-import Button from 'components/Button';
-import styled from 'styled-components';
-import Box from './components/Box';
-
-
 
 function App() {
   return (
-    <Box>
-      <Button>你啊好</Button>
-    </Box>
+      <div>hello </div>
   );
 }
 
