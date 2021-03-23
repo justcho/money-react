84f031909aa8e63dab30e547c0ea60add2e6901d
justchoe <1620845677@qq.com>
2021-03-23 18:04:38 +0800

    add react router; checkout HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
+ function App() {
   return (
-      <div>hello </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h3>
+        页面不存在，输错地址了
+      </h3>
+    </div>
   );
 }
 
