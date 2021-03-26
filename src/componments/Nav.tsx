9f6f31cd88a4b0ce7f5952b7bd9f71287be1a27b
justchoe <1620845677@qq.com>
2021-03-26 17:03:53 +0800

    repair Nav click area

--- src/componments/Nav.tsx
+++ src/componments/Nav.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Icon from './Icon';
 
 
-
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
@@ -15,14 +14,18 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.33333%;
       text-align: center;
-      display: flex;
-      flex-direction: column;
-      padding: 4px 0;
-      justify-content: center;
-      align-items: center;
-      .icon{
-        width: 24px;
-        height: 24px;
+
+      > a {
+        display: flex;
+        flex-direction: column;
+        padding: 4px 0;
+        justify-content: center;
+        align-items: center;
+
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
   }
@@ -32,16 +35,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-         <Icon name="tag"/>
-          <Link to="/tags">标签页</Link>
+          <Link to="/tags">
+            <Icon name="tag"/>
+            标签页
+          </Link>
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账页</Link>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账页
+          </Link>
         </li>
         <li>
-          <Icon name="chart"/>
-          <Link to="/statistics">统计页</Link>
+          <Link to="/statistics">
+            <Icon name="chart"/>
+            统计页
+          </Link>
         </li>
       </ul>
     </NavWrapper>
